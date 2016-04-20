618acf95d5c2dfe69ea9ff69b84b5685180cbfd1
Alex Lau <429095816@qq.com>
2016-04-20 16:23:18 +0800

    if - else

---
+++ if-else/if-else/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  if-else
+//
+//  Created by lwb on 16/4/20.
+//  Copyright © 2016年 lwb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int number_to_test,remainder;
+        NSLog(@"Enter your number to be tested: ");
+        scanf("%i",&number_to_test);
+        remainder = number_to_test % 2;
+        if(remainder == 0)
+            NSLog(@"The number is even");
+        if(remainder != 0)
+            NSLog(@"The number is odd.");
+        
+    }
+    return 0;
+
+}
